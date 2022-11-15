e5fa1b08d0730fa0094e4f09b466cff8938d1273
grigorijtesko <113973185+grigorijtesko@users.noreply.github.com>
2022-11-15 09:21:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ dino.classList.remove("jump")
     },500);
 }
 
+ var score = 0;
+
 var isAlive = setInterval (function() {
     var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140) {
-        alert("Game Over")
+        alert("Game Over");
+        score=0;
     }
 }, 10)
 
@@ -31,7 +34,7 @@ var cactusSkin = setInterval(function() {
     document.getElementById("cactus").classList.add(cactuses[Math.floor((Math.random() * 2))]);
 }, 3000)
 
- var score = 0;
+
  var scoreUpdate = setInterval(function(){
     score = score + 1;
     document.getElementById("score").innerHTML=score;
